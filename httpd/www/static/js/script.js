3e3b0c1d2a7d428004bd81d95ed0da09dbd4e594
Hagen Paul Pfeifer <hagen@jauu.net>
2017-03-02 23:49:04 +0100

    make nodes/edges dynamic
    Signed-off-by: Hagen Paul Pfeifer <hagen@jauu.net>

--- httpd/www/static/js/script.js
+++ httpd/www/static/js/script.js
@@ -1,3 +1,4 @@
+// http://visjs.org/examples/network/data/dynamicData.html
 window.onload = function() {
 var container = document.getElementById('mynetwork');
 
@@ -5,8 +6,8 @@ var nodes = null;
 var edges = null;
 var network = null;
 
-
-nodes = [
+nodes = new vis.DataSet();
+nodes.add([
 {id: 1,  value: 3,  label: 'R1' , shadow:true},
 {id: 2,  value: 2, label: 'R1T1', shadow:true},
 {id: 3,  value: 2, label: 'R1T2', shadow:true},
@@ -19,10 +20,11 @@ nodes = [
 {id: 8,  value: 2, label: 'R1T1', shadow:true},
 {id: 9,  value: 2, label: 'R1T2', shadow:true}
 
-];
+]);
 
 
-edges = [
+edges = new vis.DataSet();
+edges.add([
 {from: 1, to: 2, value: 6, title: '<b>link1</b>ff', shadow:true, color: "#ff0000"},
 {from: 1, to: 3, value: 6, title: 'link1', shadow:true, color: "#ff0000"},
 
@@ -40,7 +42,7 @@ edges = [
 
 {from: 2, to: 8, value: 4, title: 'link2', shadow:true},
 {from: 3, to: 9, value: 5, title: 'link2', shadow:true}
-];
+]);
 
 
 var container = document.getElementById('mynetwork');
@@ -61,4 +63,85 @@ nodes: {
 };
 
 network = new vis.Network(container, data, options);
+
+function getRandomInt(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+function addNode() {
+		try {
+				nodes.add({
+						id: getRandomInt(10, 200),
+						label: getRandomInt(10, 200)
+				});
+		}
+		catch (err) {
+				alert(err);
+		}
+}
+
+
+function updateNode() {
+		try {
+				nodes.update({
+						id: document.getElementById('node-id').value,
+						label: document.getElementById('node-label').value
+				});
+		}
+		catch (err) {
+				alert(err);
+		}
+}
+function removeNode() {
+		try {
+				nodes.remove({id: document.getElementById('node-id').value});
+		}
+		catch (err) {
+				alert(err);
+		}
+}
+
+function addEdge() {
+		try {
+				edges.add({
+						id: document.getElementById('edge-id').value,
+						from: document.getElementById('edge-from').value,
+						to: document.getElementById('edge-to').value
+				});
+		}
+		catch (err) {
+				alert(err);
+		}
+}
+function updateEdge() {
+		try {
+				edges.update({
+						id: document.getElementById('edge-id').value,
+						from: document.getElementById('edge-from').value,
+						to: document.getElementById('edge-to').value
+				});
+		}
+		catch (err) {
+				alert(err);
+		}
+}
+function removeEdge() {
+		try {
+				edges.remove({id: document.getElementById('edge-id').value});
+		}
+		catch (err) {
+				alert(err);
+		}
+}
+
+function timeout() {
+    setTimeout(function () {
+				addNode();
+        timeout();
+    }, 1000);
+}
+
+timeout();
+
+
 };
